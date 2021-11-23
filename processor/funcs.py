81a9e1101169fee0404599df109c51876365cc1e
KT Hobbs <38924387+KHobbs3@users.noreply.github.com>
2021-11-23 12:00:31 -0800

    Updated after testing.

--- processor/funcs.py
+++ processor/funcs.py
@@ -1,16 +1,13 @@
 # Functions used in jsonprocessor.py.
 import pandas as pd
-import re
+# import re
+
 
 def codeConverter(value):
-        """Converts province or territory 2-letter alpha code to corresponding SGC code."""
-        d = pd.read_csv("input/prov-terr.csv").set_index("Alpha_codes")['SGC_code'].to_dict()
-        return d[value]
+    """Converts province or territory 2-letter alpha code to corresponding SGC code."""
+    d = pd.read_csv("input/prov-terr.csv").set_index("Alpha_codes")['SGC_code'].to_dict()
+    return d[value]
 
-def isForceValue(value):
-        """Returns True if value contains the prefix 'force:'."""
-        FORCE_REGEXP = re.compile(r'force:.*', re.UNICODE)
-        return bool(FORCE_REGEXP.match(value))
 
 def drop_cols(sourcemap, geodataframe):
     """Creates a geodataframe only retaining only field names referred to in the source map JSON file."""
@@ -24,3 +21,111 @@ def drop_cols(sourcemap, geodataframe):
                 cols.append(k)
 
     return geodataframe.drop(geodataframe.columns.difference(cols), 1)
+
+
+def process_columns(sourcemap, geodataframe, new_class):
+    # Rename columns according to schema
+    if sourcemap['filetype'] == 'csv':
+        cols_dict = {v: k for k, v in sourcemap['schema'].items()} # Replace csv column names with varmap field names
+        cols_dict.pop(sourcemap['schema']['geometry']) # New geometry column made during conversion to gdf
+    else:
+        cols_dict = {v: k for k, v in sourcemap['schema'].items()}
+
+    geodataframe.rename(columns=cols_dict, inplace = True)
+
+    # Drop unnessary columns
+    geodataframe = drop_cols(sourcemap, geodataframe)
+
+    # Set forced columns as fixed values
+    for key, value in sourcemap['force'].items():
+        geodataframe[key] = value
+
+    # Add class column
+    geodataframe['class'] = new_class
+
+    # Return
+    return geodataframe
+
+
+export_dict = {
+    'walk_value': 'walking',
+    'bike_value': 'biking',
+    'multi_value': 'multi'
+}
+
+
+def multi(jsonFile, sourcemap, geodataframe):
+    """
+    Filters geodataframe with a "multi-use" class
+    according to walking, biking, multi column filters, if filled.
+
+    Exports to appropriate biking or walking output subfolders.
+
+    Where no filtering is required, multi-use file is exported twice (once to each subdirectory)/
+    """
+    if bool(sourcemap['filter'].keys()):
+        for key in sourcemap['filter'].keys():
+            if key in ["walk_column", "bike_column", "multi_column"] and sourcemap['filter'][key] != "":
+                k = sourcemap['filter'][key]
+            # Can we do something here like make dict pairs of each col,val and refer to those?
+            if key in ["walk_value", "bike_value", "multi_value"] and sourcemap['filter'][key] != "":
+                v = sourcemap['filter'][key]
+
+                # Filter & export subsets
+                if export_dict[key] == "walking":
+                    # Filter by several or one value(s)
+                    if "__" in v:
+                        v = v.split("__")
+                        walking = geodataframe.loc[geodataframe[k].map(lambda x: x in v)]
+                    elif "contains" in v.lower():
+                        v = v.lower().split("contains ")[1]
+                        walking = geodataframe[k].str.contains(v)
+
+                    else:
+                        walking = geodataframe.loc[geodataframe[k].map(lambda x: x == v)]
+
+                    print("Exporting filtered walking data...")
+                    process_columns(sourcemap, walking, new_class = "walking")\
+                        .to_file(f'output/walking/{jsonFile.split("/")[-1].split(".")[0]}.geojson', driver="GeoJSON")
+
+                elif export_dict[key] == "biking":
+
+                    # Filter by several or one value(s)
+                    if "__" in v:
+                        v = v.split("__")
+                        biking = geodataframe.loc[geodataframe[k].map(lambda x: x in v)]
+
+                    elif "contains" in v.lower():
+                        v = v.lower().split("contains ")[1]
+                        biking = geodataframe[k].str.contains(v)
+
+                    else:
+                        biking = geodataframe.loc[geodataframe[k].map(lambda x: x == v)]
+
+                    print("Exporting filtered biking data...")
+                    process_columns(sourcemap, biking, new_class = "biking")\
+                        .to_file(f'output/biking/{jsonFile.split("/")[-1].split(".")[0]}.geojson', driver="GeoJSON")
+
+                elif export_dict[key] == "multi":
+                    print("Exporting filtered multi data...")
+
+                    # Filter by several or one value(s)
+                    if "__" in v:
+                        v = v.split("__")
+                        multi = geodataframe.loc[geodataframe[k].map(lambda x: x in v)]
+
+                    elif "contains" in v.lower():
+                        v = v.lower().split("contains ")[1]
+                        multi = geodataframe[k].str.contains(v)
+
+                    else:
+                        multi = geodataframe.loc[geodataframe[k].map(lambda x: x == v)]
+
+
+                    process_columns(sourcemap, multi, new_class = "multi-use")\
+                        .to_file(f'output/multi/{jsonFile.split("/")[-1].split(".")[0]}.geojson', driver="GeoJSON")
+
+    else:
+        print("Exporting multi data...")
+        process_columns(sourcemap, geodataframe, new_class = "multi-use")\
+            .to_file(f'output/multi/{jsonFile.split("/")[-1].split(".")[0]}.geojson', driver="GeoJSON")
--- processor/jsonprocessor.py
+++ processor/jsonprocessor.py
@@ -3,7 +3,7 @@ Conversion of transportation source files using JSON schemas to map columns.
 
 Input: JSON source file (from which data file is read)
     Supported readable file types:
-        * CSV
+        * CSV (geom_types must be multilinestrings)
         * JSON
         * GeoJSON
         * Shapefile
@@ -11,83 +11,83 @@ Input: JSON source file (from which data file is read)
 Output: JSON data file
 Usage: `python jsonprocessor.py ../generator/json/filename`
 
+
+TODO before running:
+    -- Copy data from minio (./cp_input.sh)
+    -- Rearrange file order (Remove CD/CSD from file path and move to input/)
+    -- Move variablemap.csv to generator/
 """
 import sys
 import json
 import pandas as pd
 import geopandas as gpd
-from funcs import *
+import funcs
+from shapely.geometry import Point
+
 
 # Read source mappings from bash argument
 jsonFile = sys.argv[1]
 with open(jsonFile, "r") as f:
-    print("Converting JSON encoded data into Python dictionary.")
+    print("Converting JSON encoded data into Python dictionary...")
     srcmap = json.load(f)
 
 
-# Path to files --- SUBJECT TO CHANGE WITH VAR MAP
-reg = srcmap['prov/terr'].split(":")[1].upper()
-foldername = "{} - {}".format(codeConverter(reg), reg.upper())
-fpath = "input/{}/{}/".format(foldername, srcmap['municipality'].title().replace(' ', '').split(":")[1])
+# Path to files
+reg = srcmap['force']['prov/terr'].upper()
+foldername = f"{funcs.codeConverter(reg)} - {reg.upper()}"
+
+## Subfolder dict for class
+# TODO: convert variety in variablemaps to standards
+d = {
+    'multi-use' : 'Mixed', 
+    'multi' : 'Mixed',
+    'mixed' : 'Mixed',
+    'walking' : 'Pedestrian',
+    'cycling' : 'Cycling',
+    'biking' : 'Cycling'
+    }
+subfolder = d[srcmap['class']]
+
+# fpath = f"~/INFC/input/temp_files/{foldername}/*/{srcmap['force']['municipality'].title().replace(' ', '')}/*/"
+fpath = f"input/{foldername}/{srcmap['force']['municipality']}/{subfolder}/"
 
 
-# Read file according to file type and convert to GeoDataFrame
-if srcmap['filetype'] == 'csv':
-    df = pd.read_csv(fpath + srcmap['filename'])
-    gdf = geopandas.GeoDataFrame(df,
-        geometry=srcmap['schema']['address']['geometry'])
-
-elif srcmap['filetype'] == 'json':
+# Read file according to file type, convert to GeoDataFrame, and set/reproject crs
+if srcmap['filetype'] == 'json':
     df = pd.read_json(fpath + srcmap['filename'])
-    gdf = geopandas.GeoDataFrame(df,
-        geometry=srcmap['schema']['address']['geometry'])
+    gdf = gpd.GeoDataFrame(df, crs="EPSG:4326",
+                           geometry=srcmap['schema']['geometry'])
 
-elif srcmap['filetype'] == 'shp':
+elif srcmap['filetype'] == 'csv':
+    df = pd.read_csv(fpath + srcmap['filename'])
+    geometry = gpd.GeoSeries.from_wkt(df[srcmap['schema']['geometry']])
+    gdf = gpd.GeoDataFrame(df, crs = 'EPSG:4326',
+                           geometry = geometry)
+
+elif srcmap['filetype'] in ['shp', 'geojson']:
     gdf = gpd.read_file(fpath + srcmap['filename'])
-
-elif srcmap['filetype'] == 'geojson':
-    gdf = gpd.read_file(fpath + srcmap['filename'], driver = "GeoJSON")
+    gdf = gdf.to_crs("EPSG:4326")
 
 else:
-    print("File type % not supported." % srcmap['filetype'])
+    print(f"File type {srcmap['filetype']} not supported.")
 
 
-# Rename columns according to schema
-cols_dict = {v: k for k, v in srcmap['schema'].items()}
-gdf.rename(columns = cols_dict, inplace = True)
+# Filter out non-operational/inactive ----
+if 'inscope_filter' in srcmap['filter'].keys():
+    gdf.loc[gdf[f"{srcmap['filter']['inscope_filter']}"] == f"{srcmap['filter']['inscope_value']}"]
 
 
-# Set forced columns as fixed values
-for key, value in srcmap.items():
-    try:
-        if isForceValue(value):
-            try:
-                gdf[key] = value.split(':')[1] + value.split(':')[2]
-            except IndexError:
-                gdf[key] = value.split(':')[1]
-    except TypeError:
-        continue
+# Subset, clean columns & export as GeoJSON
+if srcmap['class'] == 'multi-use':
+    funcs.multi(jsonFile, srcmap, gdf)
 
-# filter data
-    #TODO: update logic based on variablemap.csv ----
-if srcmap['separate']:
-    gdf.query('{} == {}'\
-              .format(srcmap['separate']['filter_column'], srcmap['separate']['filter_value']),
-                  inplace = True)
+if srcmap['class'] == 'biking':
+    gdf = funcs.process_columns(srcmap, gdf, new_class = 'biking')
+    gdf.to_file(f'output/biking/{jsonFile.split("/")[-1].split(".")[0]}.geojson', driver="GeoJSON")
+
+if srcmap['class'] == 'walking':
+    gdf = funcs.process_columns(srcmap, gdf, new_class = 'walking')
+    gdf.to_file(f'output/walking/{jsonFile.split("/")[-1].split(".")[0]}.geojson', driver="GeoJSON")
 
 
-# Drop unnessary columns
-gdf = drop_cols(srcmap, gdf)
-
-
-# Add class column
-if type(srcmap['class']) == list():
-    # TODO: duplicate entire data set? ----
-else:
-    df['class'] = srcmap['class']
-
-
-# Export as GeoJSON
-    #TODO: find better way to name out files ----
-gdf.to_file('output/{}.json'.format(jsonFile.split("/")[3].split('.')[0]))
 print("Done.")
